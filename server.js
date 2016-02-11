0da7af3e5057c07f506638ac40543a3e3449baad
smhasanmansur <smhasanmansur@gmail.com>
2016-02-11 18:40:20 +0600

    broadcasting to browser clients

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 8080 });
 
+var browserClients = new Object();
+
 wss.on('connection', function connection(ws) {
     console.log('Connection open');
-	//'message' listener
+    if(ws.upgradeReq.url.substr(1)){
+        var key = ws.upgradeReq.url.substr(1);
+        browserClients[key] = ws;
+    }
     ws.on('message', function message(message) {
         switch(message){
             case 'ready':
                 console.log('client: ' + message);
+                //broadcast to browser clients
+                for (var client in browserClients) {
+                    browserClients[client].send('client: ' + message);
+                }
+                break;
+            case 'browser ready':
+                console.log(message);
                 break;
             default:
                 console.log('received: ' + message);
+                //broadcast to browser clients
+                for (var client in browserClients) {
+                    browserClients[client].send('received: ' + message);
+                }
                 break;
         }     
     });
