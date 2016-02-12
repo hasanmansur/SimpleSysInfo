aed163437637854d5bfb21c29166f4f91f3f3358
smhasanmansur <smhasanmansur@gmail.com>
2016-02-13 01:04:17 +0600

    sending msg to browser client in try/catch

--- server.js
+++ server.js
@@ -3,19 +3,27 @@ var wss = new WebSocketServer({ port: 8080 });
 
 var browserClients = new Object();
 
+//'connection' listener
 wss.on('connection', function connection(ws) {
     console.log('Connection open');
     if(ws.upgradeReq.url.substr(1)){
         var key = ws.upgradeReq.url.substr(1);
         browserClients[key] = ws;
     }
+	//'message' listener
     ws.on('message', function message(message) {
         switch(message){
             case 'ready':
                 console.log('client: ' + message);
                 //broadcast to browser clients
                 for (var client in browserClients) {
-                    browserClients[client].send('client: ' + message);
+					try {
+						browserClients[client].send('client: ' + message);
+					}
+                    catch (e) {
+						console.log(e);
+						delete browserClients[client]; 
+					}
                 }
                 break;
             case 'browser ready':
@@ -25,20 +33,29 @@ wss.on('connection', function connection(ws) {
                 console.log('received: ' + message);
                 //broadcast to browser clients
                 for (var client in browserClients) {
-                    browserClients[client].send('received: ' + message);
+					try {
+						browserClients[client].send('received: ' + message);
+					}
+                    catch (e) {
+						console.log(e);
+						delete browserClients[client];
+					}
                 }
                 break;
         }     
     });
 	//'close' listener
-    ws.on('close', function close(code, msg){
+    ws.on('close', function close(code, msg) {
         console.log('conneciton closed with code' + code);
     });
+	//'error' listener
+	ws.on('error', function error(e) {
+		console.log(e);
+	});
     ws.send('ready');
 });
 
 //'SIGINT' listener
-process.on('SIGINT', function(){
+process.on('SIGINT', function() {
     wss.close();
 });
-
