4d5deea13951bbb72746ce2eae8961734c736f31
smhasanmansur <smhasanmansur@gmail.com>
2016-02-17 01:01:43 +0600

    connection & subscribe event listeners

--- redis-publisher.js
+++ redis-publisher.js
@@ -1,8 +1,31 @@
 /**
- * Redis Client
+ * Redis Publisher Client
  */
 var redis = require("redis");
 var publisher = redis.createClient();
+
+/**
+* Connection events listener
+*/
+publisher.on("ready", function () {
+	console.log("publisher is ready");
+});
+
+publisher.on("connect", function () {
+	console.log("publisher is connected to redis server");
+});
+
+publisher.on("error", function (e) {
+	console.log(e);
+});
+
+publisher.on("reconnecting", function (r) {
+	console.log("Trying to reconnect..." + " Attempt#" + r.attempt + ", Delay: " + r.delay + "ms");
+});
+
+publisher.on("end", function () {
+	console.log("Server connection has closed");
+});
 
 if (process.argv[2]) {
     publisher.publish("test_channel", process.argv[2]);        
@@ -13,4 +36,4 @@ else {
     process.exit();
 }
 
-//publisher.publish('test_channel', 'hello');
+publisher.subscribe("another");
--- redis-subscriber.js
+++ redis-subscriber.js
@@ -1,15 +1,49 @@
+/**
+ * Redis Subscriber Client
+ */
 var redis = require("redis");
 var subscriber = redis.createClient();
 
+/**
+* Connection events listener
+*/
+subscriber.on("ready", function () {
+	console.log("subscriber is ready");
+});
+
+subscriber.on("connect", function () {
+	console.log("subscriber is connected to redis server");
+});
+
+subscriber.on("error", function (e) {
+	console.log(e);
+});
+
+subscriber.on("reconnecting", function (r) {
+	console.log("Trying to reconnect..." + " Attempt#" + r.attempt + ", Delay: " + r.delay + "ms");
+});
+
+subscriber.on("end", function () {
+	console.log("Server connection has closed");
+});
+
+/**
+* Subscribe events listener
+*/
 subscriber.on("subscribe", function (channel, count) {
-	console.log("");
-	console.log(channel);
-	console.log(count);
+	console.log("subscribes to channel: " + channel);
+	console.log("total channel subscription: " + count);
 });
 
 subscriber.on("message", function (channel, message) {
-	console.log(channel);
-	console.log(message);
+	console.log("-----------------------------------------------");
+	console.log("Channel: " + channel);
+	console.log("msg:" + message);
+	console.log("-----------------------------------------------");
+	subscriber.publish("another", "got u");
 });
 
-subscriber.subscribe("test_channel", 'another');
+/**
+* Channel subscription
+*/
+subscriber.subscribe("test_channel");
