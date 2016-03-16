adc43bae878e31d06afabe979046dd71adb01b80
smhasanmansur <smhasanmansur@gmail.com>
2016-03-16 14:21:07 +0600

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task("js-compress", function () {
+gulp.task("js-mini", function () {
+    console.log("js-mini task");
     gulp.src("*.js")
     .pipe(uglify())
     .pipe(gulp.dest("dist/"));
 });
 
-
-gulp.task("default", ["js-compress"], function () {
+gulp.task("default", ["js-mini"], function () {
     console.log("this is the default task");
+    gulp.watch("*.js", function () {
+        gulp.run("js-mini");
+    });
 });
